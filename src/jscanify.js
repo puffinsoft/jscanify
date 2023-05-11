dbfff42790bf0380bb1e640b86cd2701e41147e8
ColonelParrot <65585002+ColonelParrot@users.noreply.github.com>
2023-05-11 09:44:59 -0400

    Make distance() more succinct

--- src/jscanify.js
+++ src/jscanify.js
@@ -16,7 +16,7 @@
    * @returns distance between two points
    */
   function distance(p1, p2) {
-    return Math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2);
+    return Math.hypot(p1.x - p2.x, p1.y - p2.y);
   }
 
   class jscanify {
